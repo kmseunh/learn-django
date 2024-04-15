16c95c95e91cf393307e80871b9b8cdc0a56c2d1
kmseunh <tmdgus8779@gmail.com>
2024-04-15 17:25:02 +0900

    feat: add file & image uploads

---
+++ file_image_uploads/posts/views.py
@@ -0,0 +1,17 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+
+from .forms import PostForm
+from .models import Post
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "post.html"
+    success_url = reverse_lazy("home")
