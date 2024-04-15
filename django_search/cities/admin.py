7921ef1f6333254c60efe5643f671e41cd063552
kmseunh <tmdgus8779@gmail.com>
2024-04-15 18:00:47 +0900

    feat: add django search

---
+++ django_search/cities/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import City
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "state",
+    )
+
+
+admin.site.register(City, CityAdmin)
